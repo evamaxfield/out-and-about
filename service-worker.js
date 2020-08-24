43df859ed223e914a6b357e74b1e7758fedf3067
JacksonMaxfield <jmaxfieldbrown@gmail.com>
2020-08-24 02:05:48 +0000

    Deploying to gh-pages from master @ 05f098b4822ecc6900763418c79775f4e37bcfb9 🚀

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.7a2469d671c95618b4e7f37355e434e8.js"
+  "/out-and-about/precache-manifest.7d4e6d6b7f57cf044841f773f2c521a8.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/out-and-about/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
